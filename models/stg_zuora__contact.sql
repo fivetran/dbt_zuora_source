0010afdd2e620c73843928aad488606a96b5a0ed
Avinash Kunnath <avinash.kunnath@fivetran.com>
2023-02-28 01:38:12 -0800

    syntax

--- models/stg_zuora__contact.sql
+++ models/stg_zuora__contact.sql
@@ -38,18 +38,3 @@ final as (
 
 select * 
 from final  
-
- 
-    {"name": "id", "datatype": dbt.type_string()},
-    {"name": "account_id", "datatype": dbt.type_string()},
-    {"name": "address_1", "datatype": dbt.type_string()},
-    {"name": "address_2", "datatype": dbt.type_string()},
-    {"name": "city", "datatype": dbt.type_string()},
-    {"name": "created_date", "datatype": dbt.type_timestamp()},
-    {"name": "country", "datatype": dbt.type_string()},
-    {"name": "first_name", "datatype": dbt.type_string()},
-    {"name": "last_name", "datatype": dbt.type_string()},
-    {"name": "postal_code", "datatype": dbt.type_string()},
-    {"name": "state", "datatype": dbt.type_string()},
-    {"name": "updated_date", "datatype": dbt.type_timestamp()},
-    {"name": "work_email", "datatype": dbt.type_string()},
